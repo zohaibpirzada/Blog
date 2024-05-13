dae5b37de11b441d18b75a6e0222e27f59223bf4
zohaibpirzada <zohaibpirzada939@gmail.com>
2024-05-13 12:07:44 +0500

    make the logout button

--- blogger/blog/urls.py
+++ blogger/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- blogger/blog/views.py
+++ blogger/blog/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+
 
 def index(request):
     return render(request, 'index.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
